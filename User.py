5bff32462b2f261b6fda0ebffc65aa29fd811f04
Jborys49 <jborys49@gmail.com>
2024-05-25 23:16:25 +0200

    added more stocks, the date functionality for users, and some test methods for login

--- User.py
+++ User.py
@@ -1,9 +1,22 @@
+import datetime
 class User:
     def __init__(self, login:str, password:str,starting_funds:float):
         self.login = login
         self.password = password
         self.starting_funds =starting_funds
         self.currentfunds = starting_funds
+        self.currentdate=datetime.date(2001,1,10)
+
+    def __init__(self, login:str, password:str,starting_funds:float,date:datetime.date):
+        self.login = login
+        self.password = password
+        self.starting_funds =starting_funds
+        self.currentfunds = starting_funds
+        self.currentdate=date
+    def get_date(self):
+        return self.currentdate
+    def next_day(self):
+        self.currentdate += datetime.timedelta(days=1)
     def check_password(self, input_password:str)->bool:
         return self.password == input_password
     def get_login(self):
