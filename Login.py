838aa1a449f32267a58208e3ceda11218fefd059
Jborys49 <jborys49@gmail.com>
2024-05-28 20:12:34 +0200

    added user interaction with stocks

--- Login.py
+++ Login.py
@@ -44,46 +44,7 @@ def end_session(currentUser:User)->bool:
     pickle.dump(currentUser,file)
     file.close()
     return True
-def test1():
-    result = 0
-    for i in range(1000000):
-        result += i
-    temp_list = [x for x in range(1000)]
-    temp_dict = {i: i*i for i in range(1000)}
-    for i in temp_list:
-        result += temp_dict[i % 1000]
-    for i in range(500):
-        result *= 2
-    for i in range(500):
-        result /= 2
-    print(f"Final result of long_method_one: {result}")
 
-def test2():
-    long_string = ""
-    for i in range(10000):
-        long_string += "This is a very long and pointless string. "
-    word_count = len(long_string.split())
-    reversed_string = long_string[::-1]
-    for char in reversed_string:
-        if char == ' ':
-            word_count -= 1
-    char_sum = sum(ord(char) for char in long_string)
-    average_ascii = char_sum / len(long_string)
-    print(f"Word count: {word_count}, Average ASCII value: {average_ascii}")
-
-
-def test3():
-    num_list = [i for i in range(100000)]
-    for i in range(100000):
-        num_list[i] = num_list[i] ** 2
-    even_squares = [x for x in num_list if x % 2 == 0]
-    odd_squares = [x for x in num_list if x % 2 != 0]
-    even_sum = sum(even_squares)
-    odd_sum = sum(odd_squares)
-    total_sum = even_sum + odd_sum
-    max_even = max(even_squares) if even_squares else None
-    min_odd = min(odd_squares) if odd_squares else None
-    print(f"Total sum: {total_sum}, Max even: {max_even}, Min odd: {min_odd}")
 newUser=register_user("bingo","bango",200.0)
 #newUser=return_user("bingo","bango")
 newUser.print()
--- User.py
+++ User.py
@@ -6,6 +6,8 @@ class User:
         self.starting_funds =starting_funds
         self.currentfunds = starting_funds
         self.currentdate=datetime.date(2001,1,10)
+        self.history={}
+        self.wallet={}
 
     def __init__(self, login:str, password:str,starting_funds:float,date:datetime.date):
         self.login = login
@@ -13,7 +15,41 @@ class User:
         self.starting_funds =starting_funds
         self.currentfunds = starting_funds
         self.currentdate=date
-    def get_date(self):
+        self.wallet={}
+        self.history=[]
+    def purchase(self,name:str,price:float, nr_of_shares:int)->bool:
+        '''This method is used to purchase a number of stocks. Returns true if purchase is succesfull
+         and false when funds are insufficient. Also updates the history of user purchases'''
+        if price * nr_of_shares>self.currentfunds:
+            return False
+        else:
+            self.currentfunds-=price * nr_of_shares
+            self.history.append({'name':name,'price':price,'shares':nr_of_shares,
+                                 'date':self.currentdate,'is_purchase':True})
+            if name not in self.wallet:
+                self.wallet[name]={'shares':nr_of_shares,'paid':price*nr_of_shares}
+            else:
+                self.wallet[name]['shares']+=nr_of_shares
+                self.wallet[name]['paid']+=price*nr_of_shares
+            return True
+    def sell(self,name:str,nr_of_shares:float,price:float)->bool:
+        '''This method sells and upddates the current funds and the history of user purchases.
+        If user does not have any stocks of the required item, or not enough shares, the function returns false'''
+        if name not in self.wallet:
+            return False
+        else:
+            if self.wallet[name]['shares']<nr_of_shares:
+                return False
+            else:
+                self.history.append({'name': name, 'price': price, 'shares': nr_of_shares,
+                                     'date': self.currentdate, 'is_purchase': False})
+                self.wallet[name]['shares']-=nr_of_shares
+                self.currentfunds+=nr_of_shares*price
+                if self.wallet[name]['shares']==0:
+                    del self.wallet[name]
+                return True
+
+    def get_date(self)->datetime.date:
         return self.currentdate
     def next_day(self):
         self.currentdate += datetime.timedelta(days=1)
