f559a500ade460905acaceca9b201cb03b6e8dcf
Jborys49 <jborys49@gmail.com>
2024-05-29 20:26:46 +0200

    added Event file that will handle the events like fetching stock data and creating plots. Also added Interface (still empty though)

---
+++ Event.py
@@ -0,0 +1,60 @@
+import os
+import datetime
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+directory='Stocks'
+
+def fetch_and_save_stock_data(symbol:str, date: datetime.date)->dict:
+    # Fetch stock data from Yahoo Finance
+    stock_start=(date-datetime.timedelta(days=60)).strftime('%Y-%m-%d')
+    stock_end=date.strftime('%Y-%m-%d')
+    stock_data = yf.download(symbol, start=stock_start, end=stock_end)
+    return stock_data
+
+
+def get_plots(symbol:str,stock_data: pd.DataFrame) -> dict:
+    plots = {}
+
+    # Plot the last week of data
+    last_week_data = stock_data[-7:]
+    fig_last_week= plt.figure(figsize=(10, 5))
+    plt.plot(last_week_data.index, last_week_data['High'], label='High Price')
+    plt.plot(last_week_data.index, last_week_data['Low'], label='Low Price')
+    plt.title(f'{symbol} Stock Prices - Last Week')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plots['last_week'] = fig_last_week
+    plt.close(fig_last_week)
+
+    # Plot the last month of data
+    last_month_data = stock_data[-30:]
+    fig_last_month = plt.figure(figsize=(10, 5))
+    plt.plot(last_month_data.index, last_month_data['High'], label='High Price')
+    plt.plot(last_month_data.index, last_month_data['Low'], label='Low Price')
+    plt.title(f'{symbol} Stock Prices - Last Month')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plots['last_month'] = fig_last_month
+    plt.close(fig_last_month)
+
+    # Plot the last 2 months of data
+    last_two_months_data = stock_data[-60:]
+    fig_last_two_months = plt.figure(figsize=(10, 5))
+    plt.plot(last_two_months_data.index, last_two_months_data['High'], label='High Price')
+    plt.plot(last_two_months_data.index, last_two_months_data['Low'], label='Low Price')
+    plt.title(f'{symbol} Stock Prices - Last 2 Months')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plots['last_two_months'] = fig_last_two_months
+    plt.close(fig_last_two_months)
+
+    return plots
+#fetch_and_save_stock_data('GME',datetime.date(2005,1,1))
+#print(fetch_and_save_stock_data('GME',datetime.date(2005,1,8)))
+plots = get_plots('GME',fetch_and_save_stock_data('GME',datetime.date(2005,1,8)))
+for plot in plots:
+    print(plt)
