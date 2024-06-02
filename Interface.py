f08ee52ba1116b5766fd382cf3f12d18ada6ee09
Jborys49 <jborys49@gmail.com>
2024-06-02 20:40:51 +0200

    added date display

--- Interface.py
+++ Interface.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-import datetime
-from decimal import Decimal
 import os
 
 from User import User
@@ -10,13 +8,14 @@ from Profile import Profile
 import Event
 
 
-def next_day(frame: tk.Frame,user:User):
+def next_day(frame: tk.Frame,user:User,label:tk.Label):
     for widget in frame.winfo_children():
         widget.destroy()
     for file in os.listdir('Stock_Graphs'):
         os.unlink(os.path.join('Stock_Graphs', file))
     user.next_day()
     Event.save_user(user)
+    label.config(text=user.get_date().strftime("%Y-%d-%m"))
     tk.Label(frame, text="Click one of the buttons for functionality", font=("Arial", 32)).pack(fill=tk.BOTH,expand=1)
 def clear_bottom(frame: tk.Frame):
     for widget in frame.winfo_children():
@@ -47,14 +46,15 @@ class Interface(tk.Frame):
         top_frame.pack_propagate(False)
         top_frame.pack(side=tk.TOP,fill=tk.X,padx=10,pady=10)
 
+        # Create the bottom frame to fill the rest of the interface
+        bottom_frame = tk.Frame(self, bg="white")
+        bottom_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
         profile_button = tk.Button(top_frame, text="Profile",command=lambda:execute_profile(bottom_frame, self.user))
         profile_button.pack(side=tk.LEFT, expand=True)
 
-        # Create the bottom frame to fill the rest of the interface
-        bottom_frame = tk.Frame(self, bg="white")
-        bottom_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True,padx=10,pady=10)
 
-        day_button = tk.Button(top_frame, text="Next Day",command=lambda:next_day(bottom_frame,self.user))
+        day_button = tk.Button(top_frame, text="Next Day",command=lambda:next_day(bottom_frame,self.user,date_label))
         day_button.pack(side=tk.LEFT, expand=True)
         # Create Buy, Sell, and Profile buttons with equal spacing
         buy_button = tk.Button(top_frame, text="Buy",command=lambda:execute_buying(bottom_frame, self.user))
@@ -63,4 +63,6 @@ class Interface(tk.Frame):
         sell_button = tk.Button(top_frame, text="Sell",command=lambda:execute_selling(bottom_frame, self.user))
         sell_button.pack(side=tk.LEFT, expand=True)
 
+        date_label=tk.Label(top_frame, text=self.user.get_date().strftime("%Y-%d-%m"))
+        date_label.pack(side=tk.LEFT,expand=True)
         clear_bottom(bottom_frame)
